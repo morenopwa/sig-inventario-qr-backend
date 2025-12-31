9f98eaacd861e4c49fd3626eb5e4e63a39d3f6ce
Will <morenopwa@gmail.com>
2025-12-31 12:30:14 -0500

    rutasok

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 
 // --- CONEXIÓN A MONGODB ---
-app.use(express.static(path.join(__dirname, 'dist')));
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
