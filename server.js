795c7a31bd6e0cc3cbaef177fbab0da3f83db278
Will <morenopwa@gmail.com>
2025-12-31 11:27:24 -0500

    a lo nomral

--- server.js
+++ server.js
@@ -198,10 +198,7 @@ app.get('/api/transactions', async (req, res) => {
     } catch (err) { res.status(500).json({ error: err.message }); }
 });
 
-// Ruta de cortesía para el inicio
-app.get('/', (req, res) => {
-    res.send('🚀 Servidor de Inventario QR - Estado: ONLINE');
-});
+
 
 // --- LANZAMIENTO ---
 const PORT = process.env.PORT || 5000;
