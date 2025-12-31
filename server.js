171fc41849a3ebe27301650309e85cdde9eca6ab
Will <morenopwa@gmail.com>
2025-12-30 22:43:49 -0500

    test deploy

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(express.json());
 
 // --- CONEXIÓN A MONGODB ---
 mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log("✅ MongoDB Conectado ya tamos ESM)"))
+    .then(() => console.log("✅ MongoDB Conectado version auto deplit ESM)"))
+    
     .catch(err => console.error("❌ Error de conexión:", err));
 // --- MODELOS DE DATOS ---
 
