d33179064d719d004e4c139b0877ca5b6060bb3f
Will <morenopwa@gmail.com>
2026-01-21 01:18:30 -0500

    actualizamos

--- models/Item.js
+++ models/Item.js
@@ -7,7 +7,7 @@ const ItemSchema = new mongoose.Schema({
     category: { type: String, default: 'General' },
     stock: { type: Number, default: 0 },
     minStock: { type: Number, default: 5 },
-    unit: { type: String, enum: ['Kg', 'Pz', 'Lt', 'm', 'Unit'], default: 'Unit' },
+    unit: { type: String, enum: ['Kg', 'Pz', 'Lt', 'm', 'Unit']},
     // El historial interno para cambios rápidos de stock
     history: [{
         action: { type: String, enum: ['IN', 'OUT'] },
