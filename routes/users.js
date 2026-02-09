f991daf0aed30a92fa95d8f5a8f71f148c555411
Will <morenopwa@gmail.com>
2026-02-09 06:45:59 -0500

    actualizamos

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import User from '../models/User.js'; 
 import Attendance from '../models/Attendance.js';
+import verifyToken from '../middleware/verifyToken.js'; // <--- AÑADE ESTA LÍNEA
 
 const router = express.Router();
 
