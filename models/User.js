2e12431703adfb21544bdbd646c18b249420f1ba
Will <morenopwa@gmail.com>
2026-02-27 20:30:57 -0500

    actualizamos

--- models/User.js
+++ models/User.js
@@ -15,7 +15,7 @@ const UserSchema = new mongoose.Schema({
     additionalDaily: { type: Number, default: 0 }, // Monto fijo para Almuerzo/Cena/Pasajes
     isActive: { type: Boolean, default: true },
     weeklyBonus: { type: Number, default: 0 }, // El bono semanal,
-    birthday: { type: String },
+    birthday: { type: Date },
 }, { timestamps: true });
 
 export default mongoose.model('User', UserSchema);
